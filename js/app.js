3aa87cd609145ed4d946cfe35469cf05756c0890
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-05 06:12:40 +0600

    coments and user add

--- js/app.js
+++ js/app.js
@@ -55,6 +55,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post.comments[0].text);
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
@@ -68,7 +69,7 @@ const createPost = (post) => {
                   >
                     <img src="${post.userImage}" alt="User Picture" />
                   </a>
-                  <a href="#" class="post__user">phero</a>
+                  <a href="#" class="post__user">${post.comments[0].user}</a>
                 </div>
 
                 <button class="post__more-options">
@@ -108,14 +109,12 @@ const createPost = (post) => {
                   </button>
                 </div>
 
-                <div class="post__content">${displayContent(
-                  post.description
-                )}</div>
+                <div class="post__content">${post.comments[0].text}</div>
 
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
+                      <img src="${post.userImage}" alt="User Picture" />
                     </a>
 
                     <span>Liked by
@@ -128,9 +127,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.description}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
